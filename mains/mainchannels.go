68f8a47073d9f32bab1f32422771f3bbbff22e0b
Ac Junior 007 <junior.leticia@gmail.com>
2025-08-18 09:15:04 -0300

    Add mainchannels.go with goroutine implementation for data sending and receiving

---
+++ mains/mainchannels.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sendData(ch chan int) {
+	fmt.Println("Sending data...")
+	ch <- 42
+}
+
+func main() {
+	ch := make(chan int)
+
+	// Launch a goroutine to send data
+	go sendData(ch)
+
+	// Receive data from the channel
+	value := <-ch
+	fmt.Println("Received data:", value)
+	time.Sleep(1 * time.Second) // Wait for the goroutine to finish
+}
